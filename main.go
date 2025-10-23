7c1468a887a6e0619727a1bd45d07d0498475077
Eugen Valek <eugenvalek0209@gmail.com>
2025-10-23 13:41:05 +0300

    main realisation

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	. "lastProject1/TrafficMultiplier"
+	. "lastProject1/TripParametresCalculator"
+	. "lastProject1/WeatherParametresCalculator"
+	"time"
+)
+
+func main() {
+	calculator := PriceCalculator{
+		TrafficClient: &RealTrafficClient{}, // В продакшене используется настоящий клиент, а мы подключим структуру-заглушку для имитации его работы
+	}
+
+	price := calculator.CalculatePrice(
+		TripParameters{Distance: 8.5, Duration: 20},
+		time.Now(),
+		WeatherData{HeavyRain, 10},
+		55.751244, 37.618423,
+	)
+
+	fmt.Printf("Ваша цена: %.0f руб.\n", price)
+}
