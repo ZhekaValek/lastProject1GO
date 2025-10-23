2e6e3e8eb139bdcd8d1e8226767d7151ca1d1371
Eugen Valek <eugenvalek0209@gmail.com>
2025-10-23 12:57:34 +0300

    Realisation of weather calculator

--- WeatherParametresCalculator/Calculator.go
+++ WeatherParametresCalculator/Calculator.go
@@ -1,1 +1,30 @@
 package WeatherParametresCalculator
+
+type WeatherCondition int
+
+const (
+	Clear WeatherCondition = iota
+	Rain
+	HeavyRain
+	Snow
+)
+
+type WeatherData struct {
+	Condition WeatherCondition
+	WindSpeed int
+}
+
+func GetWeatherMultiplier(weather WeatherData) float64 {
+	coef := 1.0
+	if weather.Condition == HeavyRain {
+		coef += 0.2
+	} else if weather.Condition == Snow {
+		coef += 0.15
+	} else if weather.Condition == Rain {
+		coef += 0.125
+	}
+	if weather.WindSpeed > 15 {
+		coef += 0.1
+	}
+	return coef
+}
